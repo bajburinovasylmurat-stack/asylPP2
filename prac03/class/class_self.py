de9d832c5f8142c88d85db3f3f6a9077884d0d9f
Asylmurat <bajburinovasylmurat@gmail.com>
2026-02-17 13:20:55 +0500

    first commit

---
+++ prac03/class/class_self.py
@@ -0,0 +1,39 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def greet(self):
+    print("Hello, my name is " + self.name)
+
+p1 = Person("Emil", 25)
+p1.greet()
+
+
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def printname(self):
+    print(self.name)
+
+p1 = Person("Tobias")
+p2 = Person("Linus")
+
+p1.printname()
+p2.printname()
+
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  pass
+
+x = Student("Mike", "Olsen")
+x.printname()
---
+++ prac03/function/func_arg.py
@@ -0,0 +1,29 @@
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+
+
+def my_function(name = "friend"):
+  print("Hello", name)
+
+my_function("Emil")
+my_function("Tobias")
+my_function()
+my_function("Linus")
+
+
+def my_function(country = "Norway"):
+  print("I am from", country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+
+def my_function(animal, name):
+  print("I have a", animal)
+  print("My", animal + "'s name is", name)
+
+my_function(animal = "dog", name = "Buddy")
