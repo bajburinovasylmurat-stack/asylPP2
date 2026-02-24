ed85ff0f63674d7638707b1661babfea442efdc2
Asylmurat <bajburinovasylmurat@gmail.com>
2026-02-24 13:20:25 +0500

    first commit

---
+++ prac04/dates.py
@@ -0,0 +1,66 @@
+import datetime
+
+x = datetime.datetime.now()
+
+print(x)
+
+
+
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.year)
+print(x.strftime("%A"))
+
+
+
+import datetime
+
+x = datetime.datetime(2020, 5, 17)
+
+print(x)
+
+
+import datetime
+
+x = datetime.datetime(2018, 6, 1)
+
+print(x.strftime("%B"))
+
+
+
+
+
+#Exercise
+#1
+from datetime import datetime, timedelta
+
+def Asik():
+    t = datetime.now()
+    r = t - timedelta(days=5)
+    print(r)
+
+Asik()
+
+#ex2
+
+
+def hi():
+    today = datetime.now().date()
+    print(today - timedelta(days=1))
+    print(today)
+    print(today + timedelta(days=1))
+
+hi()
+
+
+#ex3
+def remove():
+    now = datetime.now()
+    print(now.replace(microsecond=0))
+
+remove()
+
+
---
+++ prac04/math.py
@@ -0,0 +1,47 @@
+import math
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+
+
+
+x = abs(-7.25)
+
+print(x)
+
+
+
+x = pow(4, 3)
+
+print(x)
+
+
+
+
+x = math.sqrt(64)
+
+print(x)
+
+
+#Import math library
+import math
+
+#Round a number upward to its nearest integer
+x = math.ceil(1.4)
+
+#Round a number downward to its nearest integer
+y = math.floor(1.4)
+
+print(x)
+print(y)
+
+
+
+
+x = math.pi
+print(x)
+
