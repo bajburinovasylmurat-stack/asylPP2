79668f2ce8f775eddb8a9ac9130db4e9ef72839a
Asylmurat <bajburinovasylmurat@gmail.com>
2026-03-03 12:56:32 +0500

    Add Practice5 - Python RegEx and receipt parsing examples

--- prac05/task8.py
+++ prac05/task8.py
@@ -3,6 +3,5 @@ import re
 with open("/Users/asylmuratbaiburinov/Desktop/pp2_asyl/prac05/raw.txt", "r", encoding="utf-8") as file:
     t = file.read()
 
-pattern = r'\b[A-Z][a-z]+\b'
 
-print(re.findall(pattern, t))
+print(re.findall(r'\b[A-Z][a-z]+\b', t))
