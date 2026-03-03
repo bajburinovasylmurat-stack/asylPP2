bccbfd9823d86ba571d5446aa35f3cbd5c86beeb
Asylmurat <bajburinovasylmurat@gmail.com>
2026-03-03 12:31:49 +0500

    Add Practice5 - Python RegEx and receipt parsing examples

---
+++ prac05/receipt_parser.py
@@ -0,0 +1,35 @@
+import re
+
+#Check if the string starts with "The" and ends with "Spain":
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+
+if x:
+  print("YES! We have a match!")
+else:
+  print("No match")
+
+
+
+import re
+
+txt = "hello planet"
+
+#Search for a sequence that starts with "he", followed by 0 or 1  (any) character, and an "o":
+
+x = re.findall("he.?o", txt)
+
+print(x)
+
+#This time we got no match, because there were not zero, not one, but two characters between "he" and the "o"
+
+
+import re
+
+txt = "The rain in Spain"
+
+#Find all lower case characters alphabetically between "a" and "m":
+
+x = re.findall("[a-m]", txt)
+print(x)
